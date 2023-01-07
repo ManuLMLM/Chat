8ca96ac88fa43afb6183d3ba3cee2a51de5b7141
luis manuel lópez morales <luis_20_04_99@hotmail.com>
2023-01-07 00:22:51 -0600

    Primer commit

---
+++ Chat/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Chat
+{
+    public class ChatHub: Hub
+    {
+        public async Task EnviarMensaje(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("MensajeRecibido",user,message);
+        }
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho",$"Alguien se conecta: {Context.ConnectionId}");
+        }
+    }
+}
---
+++ Chat/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Chat.Controllers
+{
+
+    public class ChatController : Controller
+    {
+        public static Dictionary<int,string> Rooms = new Dictionary<int, string>()
+        {
+            {1,"Familia" },
+            {2,"Trabajo" },
+            {3,"Amigos" },
+            {4,"Estudios" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
